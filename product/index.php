525503c0d87b93ade3599e4ddba1714f1c549658
mutooro <martin.mutooro@gmail.com>
2023-08-12 06:18:09 +0000

    n

--- product/index.php
+++ product/index.php
@@ -13,8 +13,8 @@
  	 	<form method="POST" action="register.php?invoice_number=<?php echo $_GET['invoice_number']?>">
   	  	  <table id="table" style="width: 400px; margin: auto;overflow-x:auto; overflow-y: auto;">
   	  	 <tr>
-         <td>Bar Code:</td>
-         <td><input type="text" name="bar_code" id="bar_code" size="10" placeholder="Set a bar code" required></td>
+         <td>Batch No:</td>
+         <td><input type="text" name="bar_code" id="bar_code" size="10" placeholder="Enter Batch Number" required></td>
           </tr>
           <tr id="row1">
          <td>Medicine Name:</td>
@@ -31,11 +31,11 @@
         <td><input type="number" style="width: 95px;" name="quantity">
 
              <select style="width: 95px; height: 28px; border-color: #000080" name="sell_type" > 
-                 <option value="Bot">Bot</option>
+                 <!-- <option value="Bot">Bot</option> -->
                  <option value="Stp">Stp</option>
                   <option value="Tab">Tab</option>
-		 <option value="Sachet">Sachet</option>	
-		<option value="Unit">Unit</option>
+		 <option value="Sachet">Pachet</option>	
+		<!-- <option value="Unit">Unit</option> -->
 		<option value="Tube">Tube</option>
                  </select></td>
         
@@ -53,7 +53,7 @@
         <tr>
                    <td>Company:</td>
 
-          <td><input type="text" name="company" id="company" size="10"></td>
+          <td><input type="text" name="company" id="company" placeholder="Enter manufacturer" size="10"></td>
         </tr>
        
           <tr>
@@ -69,7 +69,7 @@
         <tr><!-- For more projects: Visit codeastro.com  -->
                    <td>Profit:</td>
 
-          <td><input type="text" name="profit_price" id="profit_price"></td>
+          <td><input type="text" name="profit_price" id="profit_price" readonly></td>
         </tr>
 
         <tr>
